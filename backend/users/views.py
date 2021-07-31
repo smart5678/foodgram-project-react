e3ab57110eddbaedebf5616fecd5d962ef9d7c1f
ums <umssml@mail.ru>
2021-07-31 15:20:49 +0300

    UserViewSet from djoiser userviewset

--- backend/users/views.py
+++ backend/users/views.py
@@ -1,6 +1,5 @@
 from django.contrib.auth import get_user_model
-from rest_framework import viewsets
-from rest_framework.decorators import action
+from djoser.views import UserViewSet
 from rest_framework.permissions import IsAuthenticatedOrReadOnly
 
 from backend.paginator import ResultsSetPagination
@@ -9,27 +8,13 @@ from users.serializers import UserSerializer
 USER = get_user_model()
 
 
-class UserViewSet(viewsets.ModelViewSet):
-    model = USER
+class UserViewSet(UserViewSet):
     pagination_class = ResultsSetPagination
     serializer_class = UserSerializer
     queryset = USER.objects.all()
     permission_classes = [IsAuthenticatedOrReadOnly]
 
-    def get_instance(self):
-        return self.request.user
 
-    @action(["get", "put", "patch", "delete"], detail=False)
-    def me(self, request, *args, **kwargs):
-        self.get_object = self.get_instance
-        if request.method == "GET":
-            return self.retrieve(request, *args, **kwargs)
-        elif request.method == "PUT":
-            return self.update(request, *args, **kwargs)
-        elif request.method == "PATCH":
-            return self.partial_update(request, *args, **kwargs)
-        elif request.method == "DELETE":
-            return self.destroy(request, *args, **kwargs)
 
 
 
