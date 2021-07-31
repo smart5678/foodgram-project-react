46588298d59a790e61e9e99117eec266d97bf1aa
ums <umssml@mail.ru>
2021-07-31 22:45:54 +0300

    +unsubscribe

--- backend/users/views.py
+++ backend/users/views.py
@@ -1,5 +1,6 @@
 from django.contrib.auth import get_user_model
 from djoser.views import UserViewSet
+from rest_framework import status
 from rest_framework.decorators import action
 from rest_framework.permissions import IsAuthenticatedOrReadOnly
 from rest_framework.renderers import JSONRenderer
@@ -19,19 +20,16 @@ class UserViewSet(UserViewSet):
     queryset = USER.objects.all()
     permission_classes = [IsAuthenticatedOrReadOnly]
 
-    @action(methods=['get'], detail=True, permission_classes=[IsAuthenticatedOrReadOnly],
+    @action(methods=['get', 'delete'], detail=True, permission_classes=[IsAuthenticatedOrReadOnly],
             url_path='subscribe', url_name='subscribe')
     def set_subscribe(self, request, id=None):
-        print('hello', id, request.user)
-        Follow.objects.create(user=request.user, author_id=id)
-        serializer = SubscriberSerializer(USER.objects.get(pk=id), context={'request': request})
-        print(serializer.data)
-        return Response(serializer.data)
-
-    @action(methods=['delete'], detail=True, permission_classes=[IsAuthenticatedOrReadOnly],
-            url_path='subscribe', url_name='subscribe')
-    def set_unsubscribe(self, request, id=None):
-        pass
+        if request.method == 'GET':
+            Follow.objects.create(user=request.user, author_id=id)
+            serializer = SubscriberSerializer(USER.objects.get(pk=id), context={'request': request})
+            return Response(serializer.data)
+        else:
+            Follow.objects.get(user=request.user, author_id=id).delete()
+            return Response(status=status.HTTP_204_NO_CONTENT)
 
 
 
