9d0472b9d17f213e1d3a46c2c74ab0b29b72d0c5
Mikhail Udovichenko <umssml@mail.ru>
2021-08-04 13:18:58 +0300

    вынесены бэкэнды

---
+++ backend/recipes/backends.py
@@ -0,0 +1,37 @@
+from rest_framework.filters import BaseFilterBackend
+
+
+class IngredientFilterBackend(BaseFilterBackend):
+    """
+    Бэкэнд для фильтрации ингредиентов
+    С сортировкой по вхождению подстроки в начало затем по алфавиту
+    """
+    def filter_queryset(self, request, queryset, view):
+        name = request.query_params.get('name')
+        if name:
+            queryset = queryset.filter(name__contains=name)
+            return queryset.annotate(
+                is_start=Case(
+                    When(name__startswith=name, then=Value(1)),
+                    default=Value(2)
+                    ),
+                ).order_by('is_start')
+        return queryset
+
+
+class RecipeFilterBackend(BaseFilterBackend):
+    """
+    Бэкэнд для филтрации тэгов
+    """
+    def filter_queryset(self, request, queryset, view):
+        filter = {}
+        tags = request.query_params.getlist('tags')
+        is_favorited = request.query_params.get('is_favorited')
+        is_in_shopping_cart = request.query_params.get('is_in_shopping_cart')
+        if is_favorited and is_favorited == 'true':
+            filter['favorited__user'] = request.user
+        if tags:
+            filter['tags__slug__in'] = tags
+        if is_in_shopping_cart and is_in_shopping_cart == 'true':
+            filter['purchased__user'] = request.user
+        return queryset.filter(**filter).distinct()
--- backend/recipes/views.py
+++ backend/recipes/views.py
@@ -16,46 +16,11 @@ from cart.models import Cart
 from cart.serializers import CartRecipeSerializer
 from social.models import Favorite
 from social.serializers import FavoriteRecipeSerializer, SimpleRecipeSerializer
+from .backends import RecipeFilterBackend, IngredientFilterBackend
 from .models import Recipe, Tag, Ingredient, RecipeIngredients
 from .serializers import RecipeSerializer, TagSerializer, IngredientSerializer
 
 USER = get_user_model()
-
-
-class IngredientFilterBackend(BaseFilterBackend):
-    """
-    Бэкэнд для фильтрации ингредиентов
-    С сортировкой по вхождению подстроки в начало затем по алфавиту
-    """
-    def filter_queryset(self, request, queryset, view):
-        name = request.query_params.get('name')
-        if name:
-            queryset = queryset.filter(name__contains=name)
-            return queryset.annotate(
-                is_start=Case(
-                    When(name__startswith=name, then=Value(1)),
-                    default=Value(2)
-                    ),
-                ).order_by('is_start')
-        return queryset
-
-
-class RecipeFilterBackend(BaseFilterBackend):
-    """
-    Бэкэнд для филтрации тэгов
-    """
-    def filter_queryset(self, request, queryset, view):
-        filter = {}
-        tags = request.query_params.getlist('tags')
-        is_favorited = request.query_params.get('is_favorited')
-        is_in_shopping_cart = request.query_params.get('is_in_shopping_cart')
-        if is_favorited and is_favorited == 'true':
-            filter['favorited__user'] = request.user
-        if tags:
-            filter['tags__slug__in'] = tags
-        if is_in_shopping_cart and is_in_shopping_cart:
-            filter['purchased__user'] = request.user
-        return queryset.filter(**filter).distinct()
 
 
 class RecipeViewSet(viewsets.ModelViewSet):
